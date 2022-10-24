d591cd1c8577fb26b6c1d7444471b4b6bbcf9f7b
workanhnguyen <anhnguyenvan0910@gmail.com>
2022-10-24 17:43:06 +0700

    upgrade files

---
+++ project-management/src/main/java/commonuse/CauHinh.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package commonuse;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Scanner;
+
+/**
+ *
+ * @author ASUS
+ */
+public class CauHinh {
+    public static final Scanner scanner = new Scanner(System.in);
+    public static  final Calendar calendar = new GregorianCalendar();
+    public static final SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+    public static final Date date = new Date();
+}
