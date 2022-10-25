43d9abf6f6c4f0cbb43ff41969d950d158da0056
workanhnguyen <anhnguyenvan0910@gmail.com>
2022-10-25 20:56:26 +0700

    from Van Anh update files

---
+++ project-management/src/main/java/com/mycompany/project/management/ChooseRoleLoginPageController.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.project.management;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+/**
+ *
+ * @author ASUS
+ */
+public class ChooseRoleLoginPageController {
+    /**
+     * Quy ước:
+     * - Giá trị 0: là nhân viên
+     * - Giá trị 1: là quản lý
+     */
+    public static int loginRole = 0;
+    
+    @FXML
+    private void switchToManagerMainPage() throws IOException {
+        loginRole = 1;
+        App.setRoot("manager-main-page");
+    }
+    
+    @FXML
+    private void switchToEmployeeMainPage() throws IOException {
+        loginRole = 0;
+        App.setRoot("employee-main-page");
+    }
+}
+
