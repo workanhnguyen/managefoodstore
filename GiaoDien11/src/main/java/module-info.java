f40c33701ed934cc2afee0a8155fcbd84b083be7
workanhnguyen <anhnguyenvan0910@gmail.com>
2022-10-31 20:44:57 +0700

    Update from NB

--- GiaoDien11/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.mycompany.giaodien11 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.mycompany.giaodien11 to javafx.fxml;
-    exports com.mycompany.giaodien11;
-}
