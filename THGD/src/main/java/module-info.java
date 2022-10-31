288ed0e3264ca46aa5333c3b2dd016328a7ebb50
workanhnguyen <anhnguyenvan0910@gmail.com>
2022-10-31 21:33:02 +0700

    from Thuy

--- THGD/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.mycompany.thgd {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.base;
-    requires javafx.graphics;
-
-    opens com.mycompany.thgd to javafx.fxml;
-    exports com.mycompany.thgd;
-}
