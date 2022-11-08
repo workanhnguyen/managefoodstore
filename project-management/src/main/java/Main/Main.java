769a3416ed8161abf644df11176b38974fb963f8
workanhnguyen <anhnguyenvan0910@gmail.com>
2022-11-08 09:15:04 +0700

    Update files

--- project-management/src/main/java/Main/Main.java
+++ project-management/src/main/java/Main/Main.java
@@ -1,31 +1,8 @@
 package Main;
 
-import commonuse.CauHinh;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String filePath = "src/main/resources/database/data-nguoidung.txt";
-        File file = new File(filePath);
-
-        try {
-            Scanner fileScanner = new Scanner(file).useDelimiter("#");
-            while (fileScanner.hasNext()) {
-                int role = Integer.parseInt(fileScanner.next());
-                if (role == 1) {
-                    System.out.println("Vai trò: Quản lý");
-                }
-                String tenDangNhap = fileScanner.next();
-                String matKhau = fileScanner.next();
-                System.out.printf("Thông tin:\nHọ tên: %s %s\nNgày sinh: %s\nQuê quán: %s",
-                        fileScanner.next(), fileScanner.next(), fileScanner.next(), fileScanner.next());
-
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+        
     }
 }
